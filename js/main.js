68f7d7f2b70f7937d8394fe1534367acce29c4cb
ponta1106 <qqkz2qf9k@kind.ocn.ne.jp>
2021-09-29 18:49:43 +0900

    Add: Componentから親要素に対してデータを渡せるように実装

--- js/main.js
+++ js/main.js
@@ -1,8 +1,46 @@
 (function() {
   'use strict';
 
+  var likeComponent = Vue.extend({
+    props: {
+      message: {
+        type: String,
+        default: 'Like'
+      }
+    },
+    data: function() {
+      return {
+        count: 0
+      }
+    },
+    template: '<button @click="countUp">{{ message }} {{ count }}</button>',
+    methods: {
+      countUp: function() {
+        this.count++;
+        this.$emit('increment');
+      }
+    }
+  })
+
+  var maritozzoComponent = Vue.extend({
+    template: '<img src="maritozzo.png" id="maritozzo">',
+  })
+
   var app = new Vue({
-    el: '#app'
+    el: '#app',
+    components: {
+      'like-component': likeComponent,
+      'maritozzo-component': maritozzoComponent
+    },
+    data: {
+      total: 0
+    },
+    methods: {
+      incrementTotal() {
+        this.total++;
+      }
+    }
+
   });
-  
+
 })();
